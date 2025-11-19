42af1bd090f243c28632a9db8326e68f33f5fcfc
ximosa <ximosa@gmail.com>
2025-11-19 16:55:28 +0100

    hola2

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
 console.log('index.tsx loaded');
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.getElementById('root').innerHTML = 'Hello World';
