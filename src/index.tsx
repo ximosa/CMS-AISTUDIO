d37fc6939abc52a893f4024b2310cba70afe34b8
ximosa <ximosa@gmail.com>
2025-11-19 16:52:48 +0100

    koiu

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+console.log('index.tsx loaded');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
